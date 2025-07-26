06c1b71d60de6b812cf7ec30d031247f6f99b92c
Omage X Article Hub Studio <gianghub56@gmail.com>
2025-07-27 00:34:38 +0700

    Update Slap_Battles.lua
    Signed-off-by: Omage X Article Hub Studio <gianghub56@gmail.com>

--- File-Script/Slap_Battles.lua
+++ File-Script/Slap_Battles.lua
@@ -2472,6 +2472,111 @@ end
 })
 
 Badge1Group:AddButton({
+    Text = "Get Reflect",
+    Func = function()
+if workspace:FindFirstChild("BountyHunterRoom"):FindFirstChild("QuestObjects"):FindFirstChild("ShatteredRealityMirror"):FindFirstChild("Meshes/Mirror1_Plane.007") then
+if workspace.BountyHunterRoom.QuestObjects.ShatteredRealityMirror["Meshes/Mirror1_Plane.007"].Transparency <= 0 then
+local teleportFunc = queueonteleport or queue_on_teleport
+if teleportFunc then
+    teleportFunc([[
+        if not game:IsLoaded() then
+            game.Loaded:Wait()
+        end
+        repeat wait() until game.Players.LocalPlayer
+        wait(3)
+if game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+local Stage1 = workspace:FindFirstChild("Stage1")
+if workspace:FindFirstChild("Stage1") then
+repeat task.wait() 
+if Stage1:FindFirstChild("Lobby") and Stage1.Lobby.Portals:FindFirstChild("normal") then
+firetouchinterest(game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"), Stage1.Lobby.Portals.normal.Teleport1, 0)
+firetouchinterest(game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"), Stage1.Lobby.Portals.normal.Teleport1, 1)
+end
+if Stage1:FindFirstChild("glove") then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage1.glove:FindFirstChild("Touch").CFrame
+end
+until game:GetService("Players").LocalPlayer.Character:FindFirstChild("Evolg") or game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Evolg")
+local GloveEvolg = game:GetService("Players").LocalPlayer.Character:FindFirstChild("Evolg") or game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Evolg")
+if GloveEvolg and GloveEvolg:FindFirstChild("Glove") and GloveEvolg.Glove:FindFirstChild("HitParticles") then
+GloveEvolg.Glove:FindFirstChild("HitParticles"):Destroy()
+end
+repeat task.wait()
+if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Evolg") then
+game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Evolg").Parent = game:GetService("Players").LocalPlayer.Character
+end
+if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Evolg") and game:GetService("Players").LocalPlayer.Character.Evolg:FindFirstChild("Network") then
+for i, v in pairs(workspace:GetChildren()) do
+if v.Name:find("shadow") and v:FindFirstChild("HumanoidRootPart") then
+game:GetService("Players").LocalPlayer.Character:WaitForChild("Evolg"):WaitForChild("Network"):FireServer("Hit",v.HumanoidRootPart)
+end
+end
+end
+until Stage1:FindFirstChild("Mirror") and Stage1.Mirror:FindFirstChild("Touch") and Stage1.Mirror.Touch:FindFirstChild("TouchInterest")
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage1.Mirror:FindFirstChild("Touch").CFrame
+end
+repeat task.wait() until workspace:FindFirstChild("Stage2")
+wait(4)
+if workspace:FindFirstChild("Stage2") then
+local Stage2 = workspace:FindFirstChild("Stage2")
+if Stage2:FindFirstChild("Lobby") and Stage2.Lobby.Portals:FindFirstChild("normal") then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage2.Lobby.Portals.normal.Teleport1.CFrame
+end
+wait(0.5)
+repeat task.wait()
+if Stage2:FindFirstChild("spinner") and Stage2.spinner:FindFirstChild("Part") and Stage2.spinner.Part.Transparency < 0.5 then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage2.spinner:FindFirstChild("Part").CFrame * CFrame.new(0, 60, 0)
+wait(0.3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+end
+until Stage2:FindFirstChild("Mirror") and Stage2.Mirror:FindFirstChild("Touch") and Stage2.Mirror.Touch:FindFirstChild("TouchInterest")
+wait(0.4)
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage2.Mirror:FindFirstChild("Touch").CFrame
+wait(4)
+end
+repeat task.wait() until workspace:FindFirstChild("Stage3")
+wait(0.3)
+if workspace:FindFirstChild("Stage3") then
+wait(5)
+local Stage3 = workspace:FindFirstChild("Stage3")
+if Stage3:FindFirstChild("Lobby") and Stage3.Lobby.Portals:FindFirstChild("default") then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage3.Lobby.Portals.default.Teleport2.CFrame
+end
+end
+repeat task.wait() until workspace:FindFirstChild("BossArena")
+if workspace:FindFirstChild("BossAttackDebris") then
+workspace.BossAttackDebris:Destroy()
+end
+local Boss = workspace:FindFirstChild("BossArena")
+if Boss:FindFirstChild("Arena") and Boss.Arena:FindFirstChild("startTrigger") then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Boss.Arena:FindFirstChild("startTrigger").CFrame
+end
+repeat task.wait()
+if Boss:FindFirstChild("Arena"):FindFirstChild("mirrors") then
+	for i, v in pairs(Boss.Arena.mirrors:GetChildren()) do
+		if v:IsA("Model") and v:FindFirstChild("Meshes/Mirror1_Plane.008") and v["Meshes/Mirror1_Plane.008"].Transparency < 0.5 then
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v:FindFirstChild("Meshes/Mirror1_Plane.008").CFrame
+		end
+	end
+end
+until workspace:FindFirstChild("Rig") and workspace.Rig:FindFirstChild("Humanoid").Health <= 0
+end
+    ]])
+elseif not teleportFunc then
+Notification("Bruh, Not only executor you autoexe", _G.TimeNotify)
+end
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.BountyHunterRoom.QuestObjects.ShatteredRealityMirror["Meshes/Mirror1_Plane.007"].CFrame
+wait(0.4)
+fireproximityprompt(workspace.BountyHunterRoom.QuestObjects.ShatteredRealityMirror["Meshes/Mirror1_Plane.007"].ProximityPrompt)
+else
+Notification("You have complete 4 quest hitman (2 quest easy, 2 quest hard)", _G.TimeNotify)
+end
+end
+    end
+})
+
+Badge1Group:AddButton({
     Text = "Get Fight Guide",
     Func = function()
 local teleportFunc = queueonteleport or queue_on_teleport
@@ -2597,12 +2702,8 @@ game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Target.Character:Fi
 task.wait(0.2)
 game.ReplicatedStorage.WarpHt:FireServer(Target.Character:WaitForChild("HumanoidRootPart"))
 task.wait(0.15)
-if workspace.DEATHBARRIER.CanTouch == true then
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").DEATHBARRIER.CFrame
-else
 workspace.DEATHBARRIER.CanTouch = true
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").DEATHBARRIER.CFrame
-end
 wait(0.1)
 game:GetService("ReplicatedStorage").WLOC:FireServer()
 wait(0.2)
@@ -5269,7 +5370,7 @@ game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.workspace.Orig
 elseif Value == "Lobby" then
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-800,328,-2.5)
 elseif Value == "Hunter Room" then
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.workspace.BountyHunterRoom.Union.CFrame * CFrame.new(0,5,0)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.BountyHunterRoom.Build.Union.CFrame * CFrame.new(0,5,0)
 elseif Value == "Brazil" then
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.workspace.Lobby.brazil.portal.CFrame
 elseif Value == "Island Slapple" then
@@ -12396,6 +12497,110 @@ Plunger:FindFirstChild("plungerglove").CFrame = game.Players.LocalPlayer.Charact
 if fireclickdetector then
 fireclickdetector(Plunger:FindFirstChildOfClass("ClickDetector"))
 end
+end
+end)
+elseif game.PlaceId == 75803343514634 then
+local Window = Library:CreateWindow({
+    Title = "Shattered Reality ⚔️",
+    Center = true,
+    AutoShow = true,
+    Resizable = true,
+	Footer = "Omega X Article Hub Version: 1.0.5",
+	Icon = 83462777349222,
+	ShowCustomCursor = true,
+    NotifySide = "Right",
+    TabPadding = 2,
+    MenuFadeTime = 0
+})
+
+Tabs = {
+	Tab = Window:AddTab("Main", "rbxassetid://4370318685"),
+	["UI Settings"] = Window:AddTab("UI Settings", "rbxassetid://7733955511")
+}
+
+local Misc1Group = Tabs.Tab:AddLeftGroupbox("Badge")
+
+Misc1Group:AddButton("Get Badge", function()
+if GetBadge then return end
+GetBadge = true
+if game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+local Stage1 = workspace:FindFirstChild("Stage1")
+if workspace:FindFirstChild("Stage1") then
+repeat task.wait() 
+if Stage1:FindFirstChild("Lobby") and Stage1.Lobby.Portals:FindFirstChild("normal") then
+firetouchinterest(game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"), Stage1.Lobby.Portals.normal.Teleport1, 0)
+firetouchinterest(game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"), Stage1.Lobby.Portals.normal.Teleport1, 1)
+end
+if Stage1:FindFirstChild("glove") then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage1.glove:FindFirstChild("Touch").CFrame
+end
+until game:GetService("Players").LocalPlayer.Character:FindFirstChild("Evolg") or game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Evolg")
+local GloveEvolg = game:GetService("Players").LocalPlayer.Character:FindFirstChild("Evolg") or game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Evolg")
+if GloveEvolg and GloveEvolg:FindFirstChild("Glove") and GloveEvolg.Glove:FindFirstChild("HitParticles") then
+GloveEvolg.Glove:FindFirstChild("HitParticles"):Destroy()
+end
+repeat task.wait()
+if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Evolg") then
+game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Evolg").Parent = game:GetService("Players").LocalPlayer.Character
+end
+if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Evolg") and game:GetService("Players").LocalPlayer.Character.Evolg:FindFirstChild("Network") then
+for i, v in pairs(workspace:GetChildren()) do
+if v.Name:find("shadow") and v:FindFirstChild("HumanoidRootPart") then
+game:GetService("Players").LocalPlayer.Character:WaitForChild("Evolg"):WaitForChild("Network"):FireServer("Hit",v.HumanoidRootPart)
+end
+end
+end
+until Stage1:FindFirstChild("Mirror") and Stage1.Mirror:FindFirstChild("Touch") and Stage1.Mirror.Touch:FindFirstChild("TouchInterest")
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage1.Mirror:FindFirstChild("Touch").CFrame
+end
+repeat task.wait() until workspace:FindFirstChild("Stage2")
+wait(4)
+if workspace:FindFirstChild("Stage2") then
+local Stage2 = workspace:FindFirstChild("Stage2")
+if Stage2:FindFirstChild("Lobby") and Stage2.Lobby.Portals:FindFirstChild("normal") then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage2.Lobby.Portals.normal.Teleport1.CFrame
+end
+wait(0.5)
+repeat task.wait()
+if Stage2:FindFirstChild("spinner") and Stage2.spinner:FindFirstChild("Part") and Stage2.spinner.Part.Transparency < 0.5 then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage2.spinner:FindFirstChild("Part").CFrame * CFrame.new(0, 60, 0)
+wait(0.3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+end
+until Stage2:FindFirstChild("Mirror") and Stage2.Mirror:FindFirstChild("Touch") and Stage2.Mirror.Touch:FindFirstChild("TouchInterest")
+wait(0.4)
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage2.Mirror:FindFirstChild("Touch").CFrame
+wait(4)
+end
+repeat task.wait() until workspace:FindFirstChild("Stage3")
+wait(0.3)
+if workspace:FindFirstChild("Stage3") then
+wait(5)
+local Stage3 = workspace:FindFirstChild("Stage3")
+if Stage3:FindFirstChild("Lobby") and Stage3.Lobby.Portals:FindFirstChild("default") then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage3.Lobby.Portals.default.Teleport2.CFrame
+end
+end
+repeat task.wait() until workspace:FindFirstChild("BossArena")
+if workspace:FindFirstChild("BossAttackDebris") then
+workspace.BossAttackDebris:Destroy()
+end
+local Boss = workspace:FindFirstChild("BossArena")
+if Boss:FindFirstChild("Arena") and Boss.Arena:FindFirstChild("startTrigger") then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Boss.Arena:FindFirstChild("startTrigger").CFrame
+end
+repeat task.wait()
+if Boss:FindFirstChild("Arena"):FindFirstChild("mirrors") then
+	for i, v in pairs(Boss.Arena.mirrors:GetChildren()) do
+		if v:IsA("Model") and v:FindFirstChild("Meshes/Mirror1_Plane.008") and v["Meshes/Mirror1_Plane.008"].Transparency < 0.5 then
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v:FindFirstChild("Meshes/Mirror1_Plane.008").CFrame
+		end
+	end
+end
+until workspace:FindFirstChild("Rig") and workspace.Rig:FindFirstChild("Humanoid").Health <= 0
+Notification("Done, Get Badge (Glove, but Hitman kill)", 5)
 end
 end)
 end
