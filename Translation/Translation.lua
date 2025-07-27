e23eec5eba4cd94fcb38e823807f553930f26a0b
Omage X Article Hub Studio <gianghub56@gmail.com>
2025-07-28 02:49:27 +0700

    Update and rename vn.json to Translation.lua
    Signed-off-by: Omage X Article Hub Studio <gianghub56@gmail.com>

---
+++ Translation/Translation.lua
@@ -0,0 +1,56 @@
+return {
+	["Việt Nam"] = {
+		["Rainbow Friends"] = {
+			["Tab"] = {
+				["Main"] = "Chủ yếu",
+				["UI Settings"] = "Cài Đặt UI"
+			},
+			----------- Tab ------------ 
+			["Main 1"] = {
+				["Main"] = "Chủ yếu",
+				["Auto PickUp item"] = "Auto Nhặt Vặt phẩm",
+				["Teleport Build Structures"] = "Dịch chuyển đến Structures"
+			},
+			["Main 2"] = {
+				["Esp"] = "Định vị",
+				["Esp Item"] = "Định vị vật phẩm",
+				["Esp Monsters"] = "Định vị quái vật"
+			}
+		},
+		----------- Ink Game ------------ 
+		["Ink Game"] = {
+			["Tab"] = {
+				["Main"] = "Chủ yếu",
+				["Misc"] = "Lặt vặt",
+				["UI Settings"] = "Cài Đặt UI"
+			},
+			----------- Tab ------------ 
+			["Main 1"] = {
+				["Green Light, Red Light"] = "Đen Xanh và Đỏ",
+				["Help Player To End"] = "Cứu player về end",
+				["Auto Help Player"] = "Tự động Cứu Player",
+				
+				["Dalgona"] = "Tách kẹo",
+				["Complete Dalgona"] = "Hoàn thành tách kẹo",
+				
+				["Tug Of War / Hide & Seek"] = "Kéo co / trốn tìm",
+				["Auto Tug of War"] = "Auto Kéo",
+				["Esp Door Exit"] = "Định vị cứu thoát",
+				["Esp Key"] = "Định vị chiến khóa",
+				["Esp Player Hide"] = "Định vị thg trốn",
+				["Esp Player Seek"] = "Định vị thg săn",
+				["Auto Teleport Hide"] = "Auto dịch chuyển thg trốn",
+				["Teleport Hide"] = "dịch chuyển thg trốn",
+				["Teleport All Key"] = "dịch chuyển tất cả key",
+				
+				["Jump Rope"] = "Nhảy dây",
+				["Complete Jump Rope"] = "Hoàn thành nhảy dây",
+				
+				["Glass Bridge"] = "Cầu kính",
+				["Glass Bridge Vision"] = "Nhìn là biết fake or real",
+				["Complete Glass Bridge"] = "Hoàn Kiếm cầu kính"
+				----- Soon -----
+			}
+		}
+	}
+}
