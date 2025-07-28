003434a1f1b928ebd3e84de221d4d2806bf6887c
Omage X Article Hub Studio <gianghub56@gmail.com>
2025-07-28 10:25:52 +0700

    Update Translation.lua

--- Translation/Translation.lua
+++ Translation/Translation.lua
@@ -24,32 +24,62 @@ return {
 				["Misc"] = "Lặt vặt",
 				["UI Settings"] = "Cài Đặt UI"
 			},
-			----------- Tab ------------ 
-			["Main 1"] = {
-				["Green Light, Red Light"] = "Đen Xanh và Đỏ",
+			["Main"] = {
+				["Green Light, Red Light"] = "Đèn Xanh và Đỏ",
 				["Help Player To End"] = "Cứu player về end",
 				["Auto Help Player"] = "Tự động Cứu Player",
-				
+
 				["Dalgona"] = "Tách kẹo",
 				["Complete Dalgona"] = "Hoàn thành tách kẹo",
-				
+
 				["Tug Of War / Hide & Seek"] = "Kéo co / trốn tìm",
 				["Auto Tug of War"] = "Auto Kéo",
 				["Esp Door Exit"] = "Định vị cứu thoát",
-				["Esp Key"] = "Định vị chiến khóa",
+				["Esp Key"] = "Định vị chìa khóa",
 				["Esp Player Hide"] = "Định vị thg trốn",
 				["Esp Player Seek"] = "Định vị thg săn",
 				["Auto Teleport Hide"] = "Auto dịch chuyển thg trốn",
-				["Teleport Hide"] = "dịch chuyển thg trốn",
-				["Teleport All Key"] = "dịch chuyển tất cả key",
-				
+				["Teleport Hide"] = "Dịch chuyển thg trốn",
+				["Teleport All Key"] = "Dịch chuyển tất cả key",
+
 				["Jump Rope"] = "Nhảy dây",
 				["Complete Jump Rope"] = "Hoàn thành nhảy dây",
-				
+
 				["Glass Bridge"] = "Cầu kính",
 				["Glass Bridge Vision"] = "Nhìn là biết fake or real",
-				["Complete Glass Bridge"] = "Hoàn Kiếm cầu kính"
-				----- Soon -----
+				["Complete Glass Bridge"] = "Hoàn thành cầu kính",
+
+				["Mingle"] = "Giao lưu",
+				["Auto Mingle"] = "Auto bẻ cổ",
+
+				["Rebel"] = "Kẻ nổi loạn",
+				["WallCheck"] = "Kiểm tra tường",
+				["Aimbot Guard"] = "Nhắm mục tiêu Guard",
+				["Bring Guard"] = "Mang Guard",
+				["Inf Ammo"] = "Vô cực đạn"
+			},
+			["Misc"] = {
+				["Auto Skip"] = "Auto Bỏ qua",
+				["Speed"] = "Tốc độ",
+				["Auto Speed"] = "Auto đặt tốc độ",
+				["No Cooldown Proximity"] = "Ko hồi chiều Proximity",
+				["Noclip"] = "Xuyên tường",
+				["Inf Jump"] = "Vô cực nhảy",
+				["Teleport Player"] = "Dịch chuyển đến Player",
+				["Camlock Player / TP"] = "Khóa camera player / TP",
+				["Auto Collect Bandage"] = "Auto nhặt băng",
+				["Auto Collect Flash Bang"] = "Auto nhặt đạn choáng",
+				["Auto Collect Grenade"] = "Auto nhặt đạn nổ",
+				["Anti Fling"] = "Chống thg fling",
+				["Anti Banana"] = "Chống vỏ chuối",
+				["Anti Lag"] = "Chống Lag",
+				
+				["Esp Hight Light"] = "Công tác ánh sáng",
+				["Esp Gui"] = "Công tác nhìn từ xa",
+				["Text Size [ Gui ]"] = "Kích thước văn bản [ nhìn từ xa ]",
+
+				["Esp Name"] = "Xem tên",
+				["Esp Distance"] = "Xem khoảng cách"
 			}
 		}
 	}
